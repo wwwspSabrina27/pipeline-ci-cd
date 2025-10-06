4985aa14ee3465d0c3fd22e2257dbc9546173835
wwwspSabrina27 <00001083562435sp@al.educacao.sp.gov.br>
2025-10-06 09:21:33 -0300

    wwyw

---
+++ Calculadora.js
@@ -0,0 +1,8 @@
+// npm install --save-dev jest
+
+function somar(num1, num2) {
+    return num1 + num2;
+}
+
+module.exports = somar;
+
---
+++ Calculadora.test.js
@@ -0,0 +1,12 @@
+// executar o jest com o comando npm test
+const somar = require('./calculadora.js');
+// A função test define um caso de teste individual
+test('Soma corretamente dois numeros positivos', () => {
+  expect(somar(2, 3)).toBe(5);
+});
+test('soma corretamente um número positivo e um zero', () => {
+ expect(somar(5, 0)).toBe(5);
+});
+/*test('soma corretamente um número positivo e um número negativo', () => {
+  expect(somar(10, -5)).toBe(5);
+});*/
